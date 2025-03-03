aa99fbbd065b5aaef564240f572b403bdae82210
JoaoVitor75 <joaovitorferreira9843@gmail.com>
2025-03-03 01:02:48 -0300

    add:RHYNOVADIA

---
+++ src/main/java/net/gabrielmazzo/onecraftmod/event/ModEventBusClientEvents.java
@@ -0,0 +1,17 @@
+package net.gabrielmazzo.onecraftmod.event;
+
+import net.gabrielmazzo.onecraftmod.OneCraftMod;
+import net.gabrielmazzo.onecraftmod.entity.client.ModModelLayers;
+import net.gabrielmazzo.onecraftmod.entity.client.RhinoModel;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = OneCraftMod.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
+public class ModEventBusClientEvents {
+    @SubscribeEvent
+    public static void registerLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
+        event.registerLayerDefinition(ModModelLayers.RHINO_LAYER, RhinoModel::createBodyLayer);
+    }
+}
